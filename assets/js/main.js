6ba34ebc90aab3e2bf1eb81c203266857f504fa7
OGHache <aguustinhaedo@gmail.com>
2024-06-06 07:37:10 -0300

    Actualizacion_de_WebSite
    *Traemos ya la pagina completa porque en el primer repositorio no funcionaba algunas funciones de la pagina o perdía versiones

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+/*===== mostrar menu =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== QUITAR MENÚ MÓVIL ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // Cuando hacemos clic en cada enlace de navegación, eliminamos la clase de menú de presentación
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== DESPLAZARSE POR SECCIONES ENLACE ACTIVO ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+  sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }                                                    
+    })
+}
+window.addEventListener('scroll', scrollActive)
